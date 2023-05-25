79645b3caca5410c28d56ec81389eae537e5709e
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-05-25 01:47:05 -0400

    login and sign up created

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 import setupJWTStrategy from "./auth/index.js";
 import passport from "passport";
-
+import authRouter from "./routes/auth.js";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(morgan("tiny"));
 
 setupJWTStrategy(passport);
+
+app.use("/auth", authRouter);
 
 app.listen(3001, () => {
     console.log("Server is up");
