41b7884c94e4d29a8c53f1ac684efc73693b0a3d
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-07-24 01:02:18 -0400

    edit review route created

--- server/routes/clinic.js
+++ server/routes/clinic.js
@@ -3,167 +3,149 @@ import express from "express";
 const router = express.Router();
 
 // GET route that gets all clinics
-router.get('/userclinic', async (req, res) => {
-    try {
-        const clinics = await prisma.clinic.findMany({
-            where: {
-                userId: parseInt(req.user.id)
-            },
-        },
-
-        )
-        res.json(clinics);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Server Error" })
-    }
-
+router.get("/userclinic", async (req, res) => {
+  try {
+    const clinics = await prisma.clinic.findMany({
+      where: {
+        userId: parseInt(req.user.id),
+      },
+    });
+    res.json(clinics);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
 });
 
+// Get route to get all clinics for a certain user
+router.get("/", async (req, res) => {
+  try {
+    const clinics = await prisma.clinic.findMany({});
 
-// Get route to get all clinics for a certain user 
-router.get('/', async (req, res) => {
-    try {
-        const clinics = await prisma.clinic.findMany({
-
-        });
-
-        res.json(clinics);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Server Error" });
-    }
+    res.json(clinics);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
 });
 
-
-router.get('/:clinicId', async (req, res) => {
-    try {
-        const clinicId = req.params.clinicId
-        const clinic = await prisma.clinic.findFirst({
-            where: {
-                id: parseInt(clinicId)
-            }
-        })
-        res.status(200).json({
-            sucess: true,
-            clinic
-        })
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Server Error" });
-    }
-
+router.get("/:clinicId", async (req, res) => {
+  try {
+    const clinicId = req.params.clinicId;
+    const clinic = await prisma.clinic.findFirst({
+      where: {
+        id: parseInt(clinicId),
+      },
+    });
+    res.status(200).json({
+      sucess: true,
+      clinic,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
 });
 
-
-
-// post route 
-router.post('/', async (req, res) => {
+// post route
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     const { phonenumber, email, address, name, description, userId } = req.body;
 
     try {
-        const clinics = await prisma.clinic.create(   
-            {
-                data: {
-                    phonenumber: phonenumber,
-                    email: email,
-                    address: address,
-                    name: name,
-                    description: description,
-                    userId: req.user.id
-
-
-                },
-            });
-
-        res.status(201)
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Server Error" });
-    }
-});
-
-
-
-
-router.delete('/:clinicId', async (req, res) => {
-    try {
-      const clinicId = req.params.clinicId;
-      const clinic = await prisma.clinic.deleteMany({
-        where: { 
-            userId: req.user.id,
-          id: parseInt(req.params.clinicId)
-        }
-      });
-      
-      if (deleteClinic) {
-        const clinicList = await prisma.clinic.findMany({
-          where: {
-            userId: req.user.id,
-          }
-        });
-      res.status(200).json({
-        success: true,
-        message: "Clinic deleted successfully"
+      const clinics = await prisma.clinic.create({
+        data: {
+          phonenumber: phonenumber,
+          email: email,
+          address: address,
+          name: name,
+          description: description,
+          userId: req.user.id,
+        },
       });
 
-    } else {
-        res.status(400), json({
-          message: "Something went wrong, post could not be deleted!"
-        })
-      }
+      res.status(201);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Server Error" });
     }
-  });
-  
-  router.put('/:clinicId', async (req, res) => {
-    try {
-      const clinicId = req.params.clinicId;
-      const { phonenumber, email, address, name, description } = req.body;
-  
-      const updatedClinic = await prisma.clinic.updateMany({
+  }
+);
+
+router.delete("/:clinicId", passport.authenticate("jwt", { session: false }), async (req, res) => {
+  try {
+    const clinicId = req.params.clinicId;
+    const clinic = await prisma.clinic.deleteMany({
+      where: {
+        userId: req.user.id,
+        id: parseInt(req.params.clinicId),
+      },
+    });
+
+    if (deleteClinic) {
+      const clinicList = await prisma.clinic.findMany({
         where: {
           userId: req.user.id,
-          id: parseInt(req.params.clinicId)
         },
-        data: {
-          phonenumber: req.body.phonenumber,
-          email: req.body.email,
-          address: req.body.address,
-          name: req.body.name,
-          description: req.body.description,
-        }
       });
-  
-      if (updatedClinic) {
-        const clinicList = await prisma.clinic.findMany({
-          where: {
-            userId: req.user.id,
-          }
+      res.status(200).json({
+        success: true,
+        message: "Clinic deleted successfully",
+      });
+    } else {
+      res.status(400),
+        json({
+          message: "Something went wrong, post could not be deleted!",
         });
-  
-        res.status(200).json({
-          success: true,
-          message: "Clinic updated successfully"
-        });
-      } else {
-        res.status(400).json({
-          success: false,
-          message: "Clinic not updated. Something failed"
-        });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Server Error" });
     }
-  });
-  
-  
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
 
+router.put("/:clinicId", passport.authenticate("jwt", { session: false }), async (req, res) => {
+  try {
+    const clinicId = req.params.clinicId;
+    const { phonenumber, email, address, name, description } = req.body;
 
+    const updatedClinic = await prisma.clinic.updateMany({
+      where: {
+        userId: req.user.id,
+        id: parseInt(req.params.clinicId),
+      },
+      data: {
+        phonenumber: req.body.phonenumber,
+        email: req.body.email,
+        address: req.body.address,
+        name: req.body.name,
+        description: req.body.description,
+      },
+    });
 
+    if (updatedClinic) {
+      const clinicList = await prisma.clinic.findMany({
+        where: {
+          userId: req.user.id,
+        },
+      });
+
+      res.status(200).json({
+        success: true,
+        message: "Clinic updated successfully",
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Clinic not updated. Something failed",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
 
 export default router;
--- server/routes/review.js
+++ server/routes/review.js
@@ -4,59 +4,79 @@ const router = express.Router();
 
 //get all reviews for clinic
 router.get("/:clinicId", async (req, res) => {
-    const clinicId = req.params.clinicId;
+  const clinicId = req.params.clinicId;
 
-    const getReviews = await prisma.review.findMany({
-        where: {
-            clinicId: clinicId
-        }
-    });
+  const getReviews = await prisma.review.findMany({
+    where: {
+      clinicId: clinicId,
+    },
+  });
 
-    res.status(200).json({
-        success: true,
-        getReviews
-    });
+  res.status(200).json({
+    success: true,
+    getReviews,
+  });
 });
 
 //get specifc review
 router.get("/:reviewId", async (req, res) => {
-    const reviewId = req.params.reviewId;
+  const reviewId = req.params.reviewId;
 
-    const getReview = await prisma.review.findFirst({
-        where: {
-            id: reviewId
-        }
-    });
+  const getReview = await prisma.review.findFirst({
+    where: {
+      id: reviewId,
+    },
+  });
 
-    res.status(200).json({
-        success: true,
-        getReview
-    })
+  res.status(200).json({
+    success: true,
+    getReview,
+  });
 });
 
 //create review
-router.post("/:clinicId", async (req, res) => {
-    const clinicId = req.params.clinicId;
+router.post("/:clinicId", passport.authenticate("jwt", { session: false }), async (req, res) => {
+  const clinicId = req.params.clinicId;
 
-    const createReview = await prisma.review.create({
-        data: {
-            rating: req.body.rating,
-            comment: req.body.comment,
-            clinicId: clinicId,
-            userId: req.user.id
-        }
-    });
+  const createReview = await prisma.review.create({
+    data: {
+      rating: req.body.rating,
+      comment: req.body.comment,
+      clinicId: clinicId,
+      userId: req.user.id,
+    },
+  });
+
+  res.status(201).json({
+    success: true,
+  });
 });
 
 //edit review
-router.put("/:clinicId", async (req, res) => {
+router.put("/:clinicId", passport.authenticate("jwt", { session: false }), async (req, res) => {
+  const clinicId = req.params.clinicId;
 
-})
+  const editReview = await prisma.review.updateMany({
+    where: {
+      clinicId: clinicId,
+      userId: req.user.id
+    },
+    data: {
+      rating: req.body.rating,
+      comment: req.body.comment,
+      clinicId: clinicId,
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    editReview
+  })
+});
 
 //delete review
 router.delete("/:clinicId", async (req, res) => {
 
-})
-
+});
 
 export default router;
