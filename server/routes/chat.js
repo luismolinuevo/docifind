ccdeae4bd829130cdc287e954d8e2bf41792f6e8
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-07-31 01:12:03 -0400

    chat routes

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -5,25 +5,79 @@ const router = express.Router();
 
 //create a chatroom
 router.post("/createroom", async (req, res) => {
+  try {
+    const { userId, userTwoId } = req.body;
+    const chatroom = await prisma.chatRoom.create({
+      data: {
+        users: {
+          //this will link user one
+          connect: [{ id: Number(userId) }, { id: Number(userTwoId) }],
+        },
+      },
+      include: { users: true },
+    });
+
+    res.status(201).json({
+      success: true,
+      chatroom,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+//get all chats for a user
+router.get(
+  "/userchats",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     try {
-  
-      const { userId, userTwoId } = req.body;
-      const chatroom = await prisma.chatRoom.create({
-        data: {
+      const chatRooms = await prisma.chatRoom.findMany({
+        where: {
           users: {
-            //this will link user one
-            connect: [{ id: Number(userId) }, { id: Number(userTwoId) }],
+            some: {
+              id: parseInt(req.user.id),
+            },
           },
         },
-        include: { users: true },
+        include: {
+          messages: {
+            orderBy: {
+              createAt: "asc",
+            },
+          },
+          users: true,
+        },
       });
-  
-      res.status(201).json({
+
+      res.status(200).json({
         success: true,
-        chatroom,
+        chatRooms,
       });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Server Error" });
     }
+  }
+);
+
+//get chatroom by chatroom id
+router.get("/:id", async (req, res) => {
+  const chatId = req.params.id;
+
+  const getChat = await prisma.chatRoom.findFirst({
+    where: {
+      id: Number(chatId),
+    },
+    include: {
+      messages: true,
+      users: true
+    },
   });
+
+  res.status(200).json({
+    success: true,
+    getChat,
+  });
+});
